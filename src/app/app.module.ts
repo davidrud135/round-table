892cf7765a9032b2be4b8a572c9dc8789f8bdfd8
David RJ <david.rud135@gmail.com>
2019-09-07 14:18:42 +0300

    Imported Auth feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
 
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     MaterialModule,
     FlexLayoutModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
