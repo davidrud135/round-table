cb79ebe30f70ebd5cbf6011d36d1d75c326646ed
David RJ <david.rud135@gmail.com>
2019-09-07 15:36:52 +0300

    Added Toolbar module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [MatButtonModule, MatToolbarModule],
+  exports: [MatButtonModule, MatToolbarModule],
 })
 export class MaterialModule {}
