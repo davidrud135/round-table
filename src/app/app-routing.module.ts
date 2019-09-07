41a13417cb2513f7c81a566a1aae3c82c1cc909b
David RJ <david.rud135@gmail.com>
2019-09-07 14:31:38 +0300

    Added default route for Chatroom component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ChatroomComponent } from './chatroom/chatroom.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: ChatroomComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
