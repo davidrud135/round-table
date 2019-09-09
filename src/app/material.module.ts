323e3cab7f18dfdf58442fe028268061bfeca79b
David RJ <david.rud135@gmail.com>
2019-09-09 23:04:54 +0300

    Added required modules for auth pages

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule],
-  exports: [MatButtonModule, MatToolbarModule],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+  ],
 })
 export class MaterialModule {}
