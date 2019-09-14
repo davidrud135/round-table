9db697104941a3b930feea255f669ed0de1d573a
David RJ <david.rud135@gmail.com>
2019-09-14 16:17:20 +0300

    Imported Material ProgressSpinner module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,7 +7,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @NgModule({
   imports: [
     MatButtonModule,
@@ -17,6 +17,7 @@ import { MatDividerModule } from '@angular/material/divider';
     MatIconModule,
     MatListModule,
     MatDividerModule,
+    MatProgressSpinnerModule,
   ],
   exports: [
     MatButtonModule,
@@ -26,6 +27,7 @@ import { MatDividerModule } from '@angular/material/divider';
     MatIconModule,
     MatListModule,
     MatDividerModule,
+    MatProgressSpinnerModule,
   ],
 })
 export class MaterialModule {}
