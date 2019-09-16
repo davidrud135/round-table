52bf4fae7f4ebeaf898b204dcaace7015a1cf52f
David RJ <david.rud135@gmail.com>
2019-09-17 01:20:38 +0300

    Added button to Header with the user's name which opens menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from './../auth/auth.service';
+import { User } from 'src/app/core/models/user.model';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  isAuthenticated: boolean;
+  userDisplayName: string;
 
-  ngOnInit() {}
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService
+      .getAuthUser()
+      .subscribe((authUser: firebase.User | null) => {
+        this.isAuthenticated = !!authUser;
+        if (this.isAuthenticated) {
+          this.authService.getCurrentUserData().subscribe((user: User) => {
+            this.userDisplayName = user.displayName;
+          });
+        }
+      });
+  }
+
+  onSignOut() {
+    this.authService.signOut();
+  }
 }
