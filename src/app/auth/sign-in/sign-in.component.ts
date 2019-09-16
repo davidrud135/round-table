27f76bc2120befe10cc5317ab9db4062cc836e81
David RJ <david.rud135@gmail.com>
2019-09-17 00:56:18 +0300

    Created reactive Sign In form with error handling and input validation

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { AuthService } from './../auth.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,7 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-  constructor() {}
+  signInForm: FormGroup;
+  isLoading = false;
 
-  ngOnInit() {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private _snackBar: MatSnackBar,
+  ) {}
+
+  ngOnInit() {
+    this.onSuccessSignIn();
+    this.signInForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      pass: new FormControl('', Validators.required),
+    });
+  }
+
+  onSignIn(): void {
+    this.isLoading = true;
+    const { email, pass } = this.signInForm.value;
+    this.authService.signIn(email, pass).catch((errMessage: string) => {
+      this._snackBar.open(errMessage, 'OK', {
+        verticalPosition: 'top',
+      });
+      this.isLoading = false;
+    });
+  }
+
+  onSuccessSignIn(): void {
+    this.authService.getAuthUser().subscribe((user: firebase.User | null) => {
+      if (user) {
+        this.isLoading = false;
+        this.router.navigateByUrl('');
+      }
+    });
+  }
 }
