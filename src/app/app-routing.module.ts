ec29a0060b09fd827e02ba2db1c7db91d005b8fb
David RJ <david.rud135@gmail.com>
2019-09-17 01:11:22 +0300

    Added Auth guard to main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ChatroomComponent } from './chatroom/chatroom.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: ChatroomComponent },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ChatroomComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
