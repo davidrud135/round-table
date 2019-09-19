3d50873035aca0688e7240815bb9d66a0615c5f1
David RJ <david.rud135@gmail.com>
2019-09-19 11:14:20 +0300

    Header has 'user' property to show his data

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { AuthService } from './../auth/auth.service';
 import { User } from 'src/app/core/models/user.model';
@@ -9,22 +10,12 @@ import { User } from 'src/app/core/models/user.model';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  isAuthenticated: boolean;
-  userDisplayName: string;
+  user$: Observable<User | null>;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.authService
-      .getAuthUser()
-      .subscribe((authUser: firebase.User | null) => {
-        this.isAuthenticated = !!authUser;
-        if (this.isAuthenticated) {
-          this.authService.getCurrentUserData().subscribe((user: User) => {
-            this.userDisplayName = user.displayName;
-          });
-        }
-      });
+    this.user$ = this.authService.getUser();
   }
 
   onSignOut() {
