c1b1cada11f8f2e43ffce34de6af24f3cc9fdcff
David RJ <david.rud135@gmail.com>
2019-09-24 00:37:27 +0300

    Imported Angular Forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AuthModule } from './auth/auth.module';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
@@ -15,7 +17,6 @@ import { ChatroomComponent } from './chatroom/chatroom.component';
 import { UsersListComponent } from './chatroom/users-list/users-list.component';
 import { ChatMessagesComponent } from './chatroom/chat-messages/chat-messages.component';
 import { ControlPanelComponent } from './chatroom/control-panel/control-panel.component';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule,
     MaterialModule,
     FlexLayoutModule,
