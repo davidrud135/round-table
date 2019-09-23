7ef3d372a739679375c9a1318b8fae90576656d2
David RJ <david.rud135@gmail.com>
2019-09-24 00:40:21 +0300

    Created field with button to send messages

--- src/app/chatroom/control-panel/control-panel.component.ts
+++ src/app/chatroom/control-panel/control-panel.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ChatService } from './../../core/chat.service';
+
 @Component({
   selector: 'app-control-panel',
   templateUrl: './control-panel.component.html',
   styleUrls: ['./control-panel.component.scss'],
 })
 export class ControlPanelComponent implements OnInit {
-  constructor() {}
+  msgFieldValue: string;
+
+  constructor(private chatService: ChatService) {}
 
   ngOnInit() {}
+
+  onMessageSend(): void {
+    const msgText = this.msgFieldValue.trim();
+    if (!msgText) {
+      return;
+    }
+    this.chatService.sendMessage(msgText);
+    this.msgFieldValue = '';
+  }
 }
