54e6e767ba2a8e920ecf5f74b6d0428dc84ab662
David RJ <david.rud135@gmail.com>
2019-09-24 00:45:16 +0300

    Added methods to send messages to DB and get users list

--- src/app/core/chat.service.ts
+++ src/app/core/chat.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import {
   AngularFirestore,
-  DocumentChangeAction,
+  AngularFirestoreDocument,
 } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, combineLatest, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { firestore } from 'firebase';
 
 import { User } from './models/user.model';
+import { Message } from './models/message.model';
 import { AuthService } from './../auth/auth.service';
+
+interface FirestoreMessage {
+  senderId: string;
+  text: string;
+  sentAt: firebase.firestore.Timestamp;
+}
+
+interface FirestoreGroupChat {
+  messages: FirestoreMessage[];
+}
 
 @Injectable({ providedIn: 'root' })
 export class ChatService {
-  private currUserId: string;
+  private groupChatDocRef: AngularFirestoreDocument;
+  public currUserId: string;
 
   constructor(
     private afStore: AngularFirestore,
@@ -22,6 +35,7 @@ export class ChatService {
         this.currUserId = user.id;
       }
     });
+    this.groupChatDocRef = this.afStore.doc('chats/group-chat');
   }
 
   public getAllUsersDataExceptOwner(): Observable<User[]> {
@@ -33,5 +47,70 @@ export class ChatService {
           return users.filter((user: User) => user.id !== this.currUserId);
         }),
       );
+  }
+
+  public sendMessage(msgText: string): Promise<void> {
+    const messageData = {
+      text: msgText,
+      senderId: this.currUserId,
+      sentAt: new Date(),
+    };
+
+    return this.groupChatDocRef.update({
+      messages: firestore.FieldValue.arrayUnion(messageData),
+    });
+  }
+
+  public getMessages(): Observable<Message[]> {
+    let groupChat: FirestoreGroupChat;
+    const usersDataMap = {};
+
+    return this.groupChatDocRef.valueChanges().pipe(
+      switchMap((chat: FirestoreGroupChat) => {
+        groupChat = chat;
+        return this.getMessagesSendersData(chat.messages);
+      }),
+      map((usersData: User[]) => {
+        usersData.forEach((userData: User) => {
+          usersDataMap[userData.id] = userData;
+        });
+        return groupChat.messages
+          .map((message: FirestoreMessage) => {
+            const { senderId, ...neededData } = message;
+            const transformedMessageData: Message = {
+              ...neededData,
+              sender: usersDataMap[senderId],
+            };
+            return transformedMessageData;
+          })
+          .sort(this.sortMessagesAscendingByTimestamp);
+      }),
+    );
+  }
+
+  private sortMessagesAscendingByTimestamp(
+    msg1: Message,
+    msg2: Message,
+  ): number {
+    return msg1.sentAt.toMillis() - msg2.sentAt.toMillis();
+  }
+
+  private getMessagesSendersData(
+    messages: FirestoreMessage[],
+  ): Observable<User[]> {
+    const usersUniqueIds: string[] = Array.from(
+      new Set(
+        messages.map((message: FirestoreMessage) => {
+          return message.senderId;
+        }),
+      ),
+    );
+    const usersDocsData$: Observable<User>[] = usersUniqueIds.map(
+      (id: string) => {
+        return this.afStore.doc<User>(`users/${id}`).valueChanges();
+      },
+    );
+
+    return usersDocsData$.length ? combineLatest(usersDocsData$) : of([]);
   }
 }
