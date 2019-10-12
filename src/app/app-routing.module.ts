a15b5cbebaa0f5ae4c4080fe85ced7f9d0cbd4f4
David RJ <david.rud135@gmail.com>
2019-10-12 21:59:47 +0300

    Added Profile page to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ChatroomComponent } from './chatroom/chatroom.component';
+import { ProfileComponent } from './profile/profile.component';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
@@ -9,6 +10,11 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: ChatroomComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
     canActivate: [AuthGuard],
   },
 ];
