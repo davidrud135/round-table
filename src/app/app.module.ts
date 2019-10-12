1af232a5667f45ca6fd767401e686d943d8cd89f
David RJ <david.rud135@gmail.com>
2019-10-12 22:00:32 +0300

    Added Firebase Storage module and profile page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AuthModule } from './auth/auth.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
@@ -17,6 +18,9 @@ import { ChatroomComponent } from './chatroom/chatroom.component';
 import { UsersListComponent } from './chatroom/users-list/users-list.component';
 import { ChatMessagesComponent } from './chatroom/chat-messages/chat-messages.component';
 import { ControlPanelComponent } from './chatroom/control-panel/control-panel.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileAvatarActionsComponent } from './core/components/profile-avatar-actions/profile-avatar-actions.component';
+import { ConfirmDialogComponent } from './core/components/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +30,11 @@ import { ControlPanelComponent } from './chatroom/control-panel/control-panel.co
     UsersListComponent,
     ChatMessagesComponent,
     ControlPanelComponent,
+    ProfileComponent,
+    ProfileAvatarActionsComponent,
+    ConfirmDialogComponent,
   ],
+  entryComponents: [ProfileAvatarActionsComponent, ConfirmDialogComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -37,6 +45,7 @@ import { ControlPanelComponent } from './chatroom/control-panel/control-panel.co
     AuthModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
